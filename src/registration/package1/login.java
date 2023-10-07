9aea72634a5b74a04a38ad84202fd97b4bb25193
prata <prata@10.0.0.222>
2023-10-06 20:10:38 -0400

    this commit used to add local repository.

---
+++ src/registration/package1/login.java
@@ -0,0 +1,16 @@
+package registration.package1;
+
+public class login {
+
+	public static void main(String[] args) {
+		int num=123;
+		String str="pratap";
+		 if(num==123&&str=="pratap") {
+			 System.out.println("log-in");
+		 }else {
+			 System.out.println("failure");
+		 }
+
+	}
+
+}
